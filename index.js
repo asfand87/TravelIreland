7a40cf60a9084b9c4a23776d25e68c15a97c03aa
asfand1987 <asfand1987@gmail.com>
2020-12-02 20:34:14 +0000

    updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var text = document.getElementsByClassName("truncated");
 
 for (var i = 0; i < text.length; i++) {
-  let truncated = text[i].innerText.substring(0, 160);
+  let truncated = text[i].innerText.substring(0, 100);
   text[i].innerText = truncated + " ...";
 }
